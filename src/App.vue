c84f6e62d1baab1d352da10bb7849091f1faeebf
Henrique <nascimentocostahenrique@gmail.com>
2021-07-14 19:31:32 -0300

    Estilizacao da pagina

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,11 @@ export default {
 </script>
 
 <style>
+html, body {
+  background-color: #000;
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
